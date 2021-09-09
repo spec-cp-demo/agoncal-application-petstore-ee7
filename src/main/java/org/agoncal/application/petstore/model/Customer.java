9f15542c0e5b4b7db3fdc2c6a400d9bc08b8078c
liorspec <90378968+liorspec@users.noreply.github.com>
2021-09-09 14:52:25 +0300

    Update Customer.java

--- src/main/java/org/agoncal/application/petstore/model/Customer.java
+++ src/main/java/org/agoncal/application/petstore/model/Customer.java
@@ -143,7 +143,6 @@ public class Customer implements Serializable
       Calendar now = new GregorianCalendar();
       now.setTime(new Date());
       int adjust = 0;
-      if (now.get(Calendar.DAY_OF_YEAR) - birth.get(Calendar.DAY_OF_YEAR) < 0)
       {
          adjust = -1;
       }
